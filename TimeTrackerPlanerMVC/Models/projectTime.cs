99a880a50c1cacc595eeca9bdd84519e7027ff83
phil4literacy <philip@learnclick.com>
2018-10-16 19:42:15 +0800

    fixed sql query

--- TimeTrackerPlanerMVC/Models/projectTime.cs
+++ TimeTrackerPlanerMVC/Models/projectTime.cs
@@ -28,20 +28,21 @@ namespace TimeTrackerPlanerMVC.Models
 
         public List<projectTime> GetProjectsTimeSpent(DateTime? dateFrom, DateTime? dateUntil)
         {
-            string sql = @"SELECT Projects.projectid, Projects.projectname, SUM(TasksDoing.duration) AS timeSpent 
+            DateTime dateFromDate = (DateTime)dateFrom;
+            DateTime dateUntilDate = (DateTime)dateUntil;
+            var starttime = dateFromDate.ToString("yyyy-MM-dd 00:00:00");
+            var endtime = dateUntilDate.AddDays(1).ToString("yyyy-MM-dd 00:00:00");
+
+            string sql = $@"SELECT Projects.projectid, Projects.projectname, SUM(TasksDoing.duration) AS timeSpent 
             FROM TasksPlanned 
             INNER JOIN TasksDoing ON TasksDoing.planid = TasksPlanned.planid
             INNER JOIN TaskNames ON TaskNames.taskid = TasksPlanned.taskid
             INNER JOIN Categories ON Categories.catid = TaskNames.categoryid
             INNER JOIN Projects ON Projects.projectid = Categories.projectid
-            WHERE TasksDoing.starttime >= '{0}' AND TasksDoing.starttime <= '{1}'
+            WHERE TasksDoing.starttime >= '{starttime}' AND TasksDoing.starttime <= '{endtime}'
             GROUP BY Projects.projectid";
 
-            DateTime dateFromDate = (DateTime)dateFrom;
-            DateTime dateUntilDate = (DateTime)dateUntil;
-            string sqlQuery = string.Format(sql, dateFromDate.ToString("yyyy-MM-dd 00:00:00"), dateUntilDate.AddDays(1).ToString("yyyy-MM-dd 00:00:00"));
-
-            return projectTimeSpent.FromSql(sqlQuery).ToList();
+            return projectTimeSpent.FromSql(sql).ToList();
         }
     }
 }
