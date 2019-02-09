ccfb80a37c84756c659f36f09e1c6ab482c10b51
phil4literacy <philip@learnclick.com>
2019-02-09 21:26:48 +0800

    adapted code, as we no longer have task names

--- ttpLaravel/app/Http/Controllers/AverageTime.php
+++ ttpLaravel/app/Http/Controllers/AverageTime.php
@@ -11,60 +11,37 @@ class AverageTime extends Controller
 {
     public function index()
     {
-        /*
-                
-        $tasksplanned = tasksplanned::with('tasknames', 'tasknames.categories', 'tasknames.categories.projects')
-        ->join('tasknames', 'tasknames.taskid', '=', 'tasksplanned.taskid')
-        ->join('categories', 'categories.catid', '=', 'tasknames.categoryid')
-        ->join('projects', 'projects.projectid', '=', 'categories.projectid')
-        ->whereDate('planneddate', '>=', '2018-12-30')
-        ->groupBy('planid', 'planneddate')
-        ->orderby('projects.projectname', 'asc')
-        ->get();
-
-        //$tasksplanned->students->sortBy('whateverProperty');
-        
-        //$taskscompleted = $tasksplanned->tasksdoing->get();
-
-        echo $tasksplanned;
-        */
-
-        echo "<hr>";
+        //DB::enableQueryLog();
 
         $tasks = DB::table('tasksdoing')
         ->join('tasksplanned', 'tasksplanned.planid', '=', 'tasksdoing.planid')
-        ->join('tasknames', 'tasknames.taskid', '=', 'tasksplanned.taskid')
-        ->join('categories', 'categories.catid', '=', 'tasknames.categoryid')
+        ->join('categories', 'categories.catid', '=', 'tasksplanned.catid')
         ->join('projects', 'projects.projectid', '=', 'categories.projectid')
-        ->selectRaw('projectname, catname, planneddate, tasksdoing.planid, tasksplanned.taskid, tasksplanned.estimation, taskname, sum(duration) as sumDuration')
-        ->groupBy('tasksdoing.planid', 'planneddate')
+        ->selectRaw('projectname, tasksplanned.catid, catname, planneddate, sum(duration) as sumDuration')
+        ->groupBy('tasksplanned.catid', 'planneddate')
         ->orderby('projects.projectname', 'asc')
-        ->whereDate('planneddate', '>=', '2018-12-30')
+        ->whereDate('planneddate', '>=', '2019-01-15')
         ->get();
 
         //dd(DB::getQueryLog());
 
         $tasksListed = array();
-        foreach($tasks as $task)
-        {
-            if(!in_array($task->taskid, $tasksListed))
-            {
-                echo "<strong>" . $task->projectname . ":</strong> " . $task->taskname . " (" . $task->catname .  ") - "; 
-                $taskdetails = $tasks->where('taskid', $task->taskid);
+        foreach ($tasks as $task) {
+            if (!in_array($task->catid, $tasksListed)) {
+                echo "<strong>" . $task->projectname . ":</strong> " . $task->catname .  " - ";
+                $taskdetails = $tasks->where('catid', $task->catid);
                 $arrDuration = array();
-                foreach($taskdetails as $taskdetail)
-                {
+                foreach ($taskdetails as $taskdetail) {
                     echo $taskdetail->sumDuration . ", ";
                     array_push($arrDuration, $taskdetail->sumDuration);
                 }
                 $averageDuration = array_sum($arrDuration)/count($arrDuration);
                 echo "(" . $averageDuration . ")";
                 echo "<br>";
-                array_push($tasksListed, $task->taskid);
+                array_push($tasksListed, $task->catid);
             }
         }
         
         //return view('averagetime', ['completedtasks' => $completedtasks]);
     }
-
 }
